4e40d3f7ff7ebb07e41aa6eb4d51ae66b9c61bc2
KeTuRr74 <avatarfightgames@gmail.com>
2018-01-01 05:11:51 -0800

    first commit

---
+++ zenlte-common-vendor.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 TeamNexus
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+define zenlte-find-vendor-blobs
+    $(foreach vendor_file, $(shell find $(1) -type f | sed -n 's|^$(1)/||p'), \
+    	$(if $(strip $(findstring $(vendor_file):$(vendor_file),$(PRODUCT_COPY_FILES))), \
+    		$(NOOP), \
+    		$(1)/$(vendor_file):$(vendor_file)))
+endef
+
+# Workaround for empty TARGET_DEVICE
+TARGET_DEVICE := $(__PRODUCT_TARGET_NAME)
+
+#
+# Blobs for SM-G92xF
+#
+ifneq ($(filter zenltexx zenlteltexx,$(TARGET_DEVICE)),)
+    PRODUCT_COPY_FILES += $(call zenlte-find-vendor-blobs,vendor/samsung/zenlte-common/devices/xx)
+endif
+
+#
+# Blobs for SM-G92xP
+#
+ifneq ($(filter zenltefltespr zenlteltespr,$(TARGET_DEVICE)),)
+    PRODUCT_COPY_FILES += $(call zenlte-find-vendor-blobs,vendor/samsung/zenlte-common/devices/spr)
+endif
+
+#
+# Blobs for non-SM-G92xP
+#
+ifeq ($(filter zenltefltespr zenlteltespr,$(TARGET_DEVICE)),)
+    PRODUCT_COPY_FILES += $(call zenlte-find-vendor-blobs,vendor/samsung/zenlte-common/devices/non-spr)
+endif
+
+#
+# Commons blobs
+#
+PRODUCT_COPY_FILES += $(call zenlte-find-vendor-blobs,vendor/samsung/zenlte-common/proprietary)
